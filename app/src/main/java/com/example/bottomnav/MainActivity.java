e76b4f931366468cc66912fd446687452a617089
ShinAquila <augusto08rivera12@gmail.com>
2024-03-04 14:13:30 +0800

    added side and bottom nav

--- app/src/main/java/com/example/bottomnav/MainActivity.java
+++ app/src/main/java/com/example/bottomnav/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.bottomnav;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.example.bottomnav.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        findViewById(R.id.imageView).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){drawerLayout.openDrawer(GravityCompat.START);}
+
+        });
+
+        //Use this code if you have a custom icons
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setItemIconTintList(null);
+
+        //Navigation Side Drawer
+        NavController navController = Navigation.findNavController(this, R.id.sideNavHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
