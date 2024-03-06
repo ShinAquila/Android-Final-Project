fc5ea8419863e5bd8a91eecbf870dd93fc37c690
ShinAquila <augusto08rivera12@gmail.com>
2024-03-06 18:42:05 +0800

    added philippines design

--- app/src/main/java/com/example/bottomnav/Philippines.java
+++ app/src/main/java/com/example/bottomnav/Philippines.java
@@ -1,14 +1,43 @@
 package com.example.bottomnav;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class Philippines extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_philippines);
+
+
+        button = (Button) findViewById(R.id.fromPhil_goBack);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){openMainActivity();}
+
+
+        });
+
     }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
