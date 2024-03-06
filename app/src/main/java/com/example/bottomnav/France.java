025d2a5cbcb98591328cdaef1ef9bd6f90d6ca40
ShinAquila <augusto08rivera12@gmail.com>
2024-03-06 19:02:05 +0800

    designed the main fragments

--- app/src/main/java/com/example/bottomnav/France.java
+++ app/src/main/java/com/example/bottomnav/France.java
@@ -2,13 +2,32 @@ package com.example.bottomnav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class France extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_france);
+
+        button = (Button) findViewById(R.id.fromFrance_goBack);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){openMainActivity();}
+
+
+        });
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bottomnav/USA.java
+++ app/src/main/java/com/example/bottomnav/USA.java
@@ -2,13 +2,33 @@ package com.example.bottomnav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class USA extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_usa);
+
+        button = (Button) findViewById(R.id.fromUSA_goBack);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){openMainActivity();}
+
+
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
